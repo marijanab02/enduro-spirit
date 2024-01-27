4c102a59a0ba2d1b34f332c707cf8818c80430ca
marijanab02 <bandicm02@gmail.com>
2024-01-27 16:57:48 +0100

    Validating registration

--- src/main/java/com/endurospirit/models/Korisnik.java
+++ src/main/java/com/endurospirit/models/Korisnik.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,26 +16,42 @@ public class Korisnik {
     @Id
     @GeneratedValue
     private Long ID;
-
+    @Size(min=3, max=20, message="Polje ime mora imati izmedu 3 i 20 znakova")
+    @NotBlank(message="Polje ime je obavezno")
     String Ime;
+    @Size(min=3, max=20, message="Polje prezime mora imati izmedu 3 i 20 znakova")
+    @NotBlank(message="Polje prezime je obavezno")
     String Prezime;
+    @NotBlank(message = "Polje email je obavezno")
+    @Email(message = "Email adresa mora biti ispravnog fonta")
     String Email;
+    @NotBlank(message = "Polje adresa je obavezno")
     String Adresa;
     String Telefon;
+    @NotBlank(message = "Polje je obavezno")
     String DatumRodenja;
+    @NotBlank(message = "Polje je obavezno")
     String VozackeSposobnosti;
+    @NotBlank(message = "Polje je obavezno")
     String VelicinaOdjece;
+    @NotNull(message = "Polje je obavezno")
     Long VelicinaObuce;
+    @NotBlank(message = "Polje je obavezno")
     String VelicinaKacige;
+    @NotNull(message = "Polje je obavezno")
     Long Tezina;
+    @NotNull(message = "Polje je obavezno")
     Long Visina;
     String Role;
+    @NotBlank(message = "Polje je obavezno")
     String Lozinka;
-
+    @NotBlank(message = "Polje je obavezno")
+    @Transient
+    String PotvrdaLozinke;
     public Korisnik() {
     }
 
-    public Korisnik(String ime, String prezime, String email, String adresa, String telefon, String datumRodenja, String vozackeSposobnosti, String velicinaOdjece, Long velicinaObuce, String velicinaKacige, Long tezina, Long visina, String role, String lozinka) {
+    public Korisnik(String ime, String prezime, String email, String adresa, String telefon, String datumRodenja, String vozackeSposobnosti, String velicinaOdjece, Long velicinaObuce, String velicinaKacige, Long tezina, Long visina, String role, String lozinka, String potvrdaLozinke) {
         Ime = ime;
         Prezime = prezime;
         Email = email;
@@ -48,6 +66,7 @@ public class Korisnik {
         Visina = visina;
         Role = role;
         Lozinka = lozinka;
+        PotvrdaLozinke = potvrdaLozinke;
     }
 
     public Long getID() {
@@ -168,6 +187,20 @@ public class Korisnik {
 
     public void setLozinka(String lozinka) {
         Lozinka = lozinka;
+    }
+    public String getPotvrdaLozinke(){
+        return PotvrdaLozinke;
+    }
+    public void setPotvrdaLozinke(String potvrdaLozinke){
+        PotvrdaLozinke=potvrdaLozinke;
+    }
+    @AssertTrue(message = "Lozinke se moraju podudarati")
+    public boolean isPasswordsEqual(){
+        try{
+            return this.Lozinka.equals(this.PotvrdaLozinke);
+        }catch (Exception e){
+            return false;
+        }
     }
     @OneToMany(mappedBy = "korisnik")
     private List<BookingLogs> bookingLogs=new ArrayList<>();
